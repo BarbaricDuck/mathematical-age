a243b14db87547430283725ea37a37b66eefacc0
Ben Groves <ben_groves2004@outlook.com>
2022-02-14 12:31:42 +0000

    ebhfbszbsdfb

---
+++ script.js
@@ -0,0 +1,11 @@
+var questions = [["Do you understand prime numbers?","20,000 BCE - Nile Valley"],
+             ["Do you know π to 2 significant figures? ","2000 BCE - Mesopotamia"],
+             ["",""],
+             ["",""],
+             ["",""],
+             ["",""],
+             ["",""],
+             ["",""],
+             ["",""],
+             ["",""]];
+console.log(questions);
